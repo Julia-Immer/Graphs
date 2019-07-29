b733effb8a8e09612b7b746db9e6a48726bea100
siag7072 <siag7072@colorado.edu>
2019-07-29 20:06:42 +0000

    initial commit

---
+++ code/Graphs.cpp
@@ -0,0 +1,10 @@
+#include "Graphs.h"
+
+// constructor, initialize class variables and pointers here if need.
+Graphs::Graphs(){
+  // your code here
+}
+
+//deconstructor,
+Graphs::~Graphs(){
+}
---
+++ code/Graphs.h
@@ -0,0 +1,29 @@
+#ifndef GRAPHS_H__
+#define GRAPHS_H__
+
+#include <string>
+#include <memory>
+#include <vector>
+#include <string>
+#include <queue>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+class Graphs {
+public:
+  Graphs();
+
+  // deconstructor, if you are using shared_pointers you don't need to keep track of
+  // freeing memory and you can leave this blank
+  ~Graphs();
+
+
+  // you can add add more public member variables and member functions here if you need
+
+private:
+  // you can add add more private member variables and member functions here if you need
+};
+
+#endif  // GRAPHS_H__
